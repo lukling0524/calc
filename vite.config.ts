5fb7c4d8cfece87be7a705a1241facc21275758f
김경화 <gyeonghwa.kim@hnine.com>
2026-01-09 12:03:02 +0900

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use '@css/abstracts' as *;
+        `,
+        api: 'modern-compiler',
+      },
+    },
+  },
+  server: {
+    port: 2000,
+  },
+});
